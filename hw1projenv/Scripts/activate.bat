687c5b974d356d2a05e16ba8582fe74cf0ceacf1
hekaiyue <yk203988@gmail.com>
2016-01-31 00:46:20 -0500

    initcommit
    skeleton home page, says hello

---
+++ hw1projenv/Scripts/activate.bat
@@ -0,0 +1,26 @@
+@echo off
+set "VIRTUAL_ENV=C:\Users\Kai\Dropbox\CMU Files\98-215 Introduction to Django\hw1\hw1projenv"
+
+if defined _OLD_VIRTUAL_PROMPT (
+    set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
+) else (
+    if not defined PROMPT (
+        set "PROMPT=$P$G"
+    )
+	set "_OLD_VIRTUAL_PROMPT=%PROMPT%"	
+)
+set "PROMPT=(hw1projenv) %PROMPT%"
+
+if not defined _OLD_VIRTUAL_PYTHONHOME (
+    set "_OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%"
+)
+set PYTHONHOME=
+
+if defined _OLD_VIRTUAL_PATH (
+    set "PATH=%_OLD_VIRTUAL_PATH%"
+) else (
+    set "_OLD_VIRTUAL_PATH=%PATH%"
+)
+set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
+
+:END
---
+++ hw1projenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\kai\dropbox\cmu files\98-215 introduction to django\hw1\hw1projenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
